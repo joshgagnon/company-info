9ba46e9052a9da8002bef213de29246896505861
Paddy Moran <pdmoran@outlook.com>
2017-01-06 00:33:21 +0000

    Added testing to company_name_history function

--- tests/unit/dbFunctions.test.js
+++ tests/unit/dbFunctions.test.js
@@ -4,21 +4,21 @@ const getDB = require('../../app/db').getDB;
 const moment = require('moment');
 
 
-describe('DB Functions', () => {
+describe('DB Functions', function() {
     let db;
     before(function (){
         db = getDB()
     });
 
-    describe('company_from_nzbn_at_date', () => {
-        it('should find the previous names of "CONSTELLATION HOLDINGS LIMITED"', () => {
+    describe('company_from_nzbn_at_date', function() {
+        it('should find the previous names of "CONSTELLATION HOLDINGS LIMITED"', function() {
             return db.func('company_from_nzbn_at_date', ['9429039824622', moment('06 July 1997', 'DD MMMM YYYY').toDate()])
                 .then((result) => {
                     result[0].company_from_nzbn_at_date.should.equal('SSANGYONG MOTOR DISTRIBUTORS NZ LIMITED');
                 });
         });
 
-        it('should find the current name of "CONSTELLATION HOLDINGS LIMITE" on day of name change', () => {
+        it('should find the current name of "CONSTELLATION HOLDINGS LIMITE" on day of name change', function() {
             return db.func('company_from_nzbn_at_date', ['9429039824622', moment('26 May 1999', 'DD MMMM YYYY').toDate()])
                 .then((result) => {
                     result[0].company_from_nzbn_at_date.should.equal('CONSTELLATION HOLDINGS LIMITED');
@@ -26,15 +26,15 @@ describe('DB Functions', () => {
         });
     });
     
-    describe('nzbn_at_date', () => {
-        it('should find nzbn for company name at date', () => {
+    describe('nzbn_at_date', function() {
+        it('should find nzbn for company name at date', function() {
             return db.func('nzbn_at_date', ['SATURN MOTORS LIMITED', moment('06 July 1992', 'DD MMMM YYYY').toDate()])
                 .then((result) => {
                     result[0].nzbn_at_date.should.equal('9429039824622');
                 });
         });
 
-        it('should find nzbn for company name at date where date is the date of a name change', () => {
+        it('should find nzbn for company name at date where date is the date of a name change', function() {
             return db.func('nzbn_at_date', ['SSANGYONG MOTOR DISTRIBUTORS NZ LIMITED', moment('05 July 1996', 'DD MMMM YYYY').toDate()])
                 .then((result) => {
                     result[0].nzbn_at_date.should.equal('9429039824622');
@@ -42,19 +42,55 @@ describe('DB Functions', () => {
         });
     });
     
-    describe('company_name_at_date', () => {
-        it('should find company name at 06 July 1992 for given company name at 06 July 1997', () => {
+    describe('company_name_at_date', function() {
+        it('should find company name at 06 July 1992 for given company name at 06 July 1997', function() {
             return db.func('company_name_at_date', ['SSANGYONG MOTOR DISTRIBUTORS NZ LIMITED', moment('06 July 1997', 'DD MMMM YYYY').toDate(),  moment('06 July 1992', 'DD MMMM YYYY').toDate()])
                 .then((result) => {
                     result[0].company_name_at_date.should.equal('SATURN MOTORS LIMITED');
                 });
         });
 
-        it('should find company name at 06 July 1992 for given company name at date of a name change', () => {
+        it('should find company name at 06 July 1992 for given company name at date of a name change', function() {
             return db.func('company_name_at_date', ['CONSTELLATION HOLDINGS LIMITED', moment('26 May 1999', 'DD MMMM YYYY').toDate(),  moment('05 July 1996', 'DD MMMM YYYY').toDate()])
                 .then((result) => {
                     result[0].company_name_at_date.should.equal('SSANGYONG MOTOR DISTRIBUTORS NZ LIMITED');
                 });
         });
     });
-})
+    
+    describe('company_name_history', function() {
+        it('should find name history of CADBURY CONFECTIONERY LIMITED (10 March 2001)', function() {
+            return db.func('company_name_history', ['CADBURY CONFECTIONERY LIMITED', moment('10 March 2001', 'DD MMMM YYYY').toDate()])
+                .then(function(actualStates) {
+                    const expectedStates = [{
+                        nzbn: '9429000001748',
+                        company_number: '204724',
+                        company_name: 'MONDELEZ NEW ZEALAND',
+                    },{
+                        nzbn: '9429000001748',
+                        company_number: '204724',
+                        company_name: 'CADBURY',
+                    },{
+                        nzbn: '9429000001748',
+                        company_number: '204724',
+                        company_name: 'CADBURY LIMITED',
+                    },{
+                        nzbn: '9429000001748',
+                        company_number: '204724',
+                        company_name: 'CADBURY CONFECTIONERY LIMITED',
+                    },{
+                        nzbn: '9429000001748',
+                        company_number: '204724',
+                        company_name: 'R. HUDSON LIMITED',
+                    }];
+
+
+                    actualStates.map(function(state, index) {
+                        state.nzbn.should.equal(expectedStates[index].nzbn);
+                        state.company_number.should.equal(expectedStates[index].company_number);
+                        state.company_name.should.equal(expectedStates[index].company_name);
+                    });
+                });
+        });
+    });
+});
